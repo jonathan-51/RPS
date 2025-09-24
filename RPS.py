57c60cfed62b9b9d8646f68523de90f574885fb5
Jonathan Li <jonathanli5150@gmail.com>
2025-09-24 16:08:52 +1200

    Version 3: Can play 'x' amount best of games

--- RPS.py
+++ RPS.py
@@ -1,70 +1,102 @@
 import random
 
-try_again = 0
-try_again_track = 0  
+Rock = "r"
+Paper = "p"
+Scissors = "s"
+options = [Rock,Paper,Scissors]
 
-while try_again == try_again_track:
-    Rock = "r"
-    Paper = "p"
-    Scissors = "s"
+def IsOdd(n): #Checks if number is odd or even
+    return n % 2 == 0
 
-    options = [Rock,Paper,Scissors]
-    comp_choice = random.choice(options)
-    comp_win_left = 2
-    user_win_left = 2
-    comp_score = 0
-    user_score = 0
+while True: #Loops the Game
+    best_of = input("Choose a format: best of 3, best of 5, best of 7....")
+    num_rounds = 0 #Tracks the number of rounds (Excluding ties)
+    comp_score = 0 #Tracks the score
+    user_score = 0 #Tracks the score
 
-    try_again = 0
-    try_again_track = 0    
-    print("Play a best of 3 against the Computer.")
-    while comp_win_left and user_win_left > 0:
+    while True: #filters out all possible responses except for odd numbers
+        try: #Converts input into an integer
+            best_of = int(best_of)
+        except:
+            best_of = input("Encountered an error.\nPlease enter an odd number: ")
 
+        if type(best_of) == int and IsOdd(best_of) == False: #Filters out the odd numbers
+            break
+        else: #filters out everything else like strings and even numbers
+            best_of = input("Encountered an error.\nPlease enter an odd number: ")
+
+    num_win_req = (best_of + 1)/2 #number of wins to win the game    
+    while user_score < num_win_req and comp_score < num_win_req: #loops the rounds
         comp_choice = random.choice(options)
         print(comp_choice)
-        print(f"comp_score = {comp_score}")
-        print(f"user_score = {user_score}")
-        print(f"comp_win_left = {comp_win_left}")
-        print(f"user_win_left = {user_win_left}")
+        #print(f"comp_score = {comp_score}")
+        #print(f"user_score = {user_score}")
+        #print(f"comp_win_left = {comp_win_left}")
+        #print(f"user_win_left = {user_win_left}")
         user_choice = input("Please Enter Your Choice (R for Rock, P for Paper, S for Scissors: ").lower()
-        
-        if user_choice == comp_choice: #Same Combination
-            print(f"Tie. Please Try Again.\nYou - {user_score}.\nComputer - {comp_score}.\n")
-        elif user_choice == Rock and comp_choice == Paper: #Rock vs Paper
-            comp_win_left += - 1
-            comp_score += 1
-            print(f"Round 1 goes to Computer.\nYou - {user_score}.\nComputer - {comp_score}.\n")
+
+        while True: #Checks to see if player entered a valid choice (r/p/s)
+            if user_choice != "r" and user_choice != "p" and user_choice != "s":
+                user_choice = input("Please try again. Enter (r/p/s): ").lower()
+            else:
+                break
+
+        if user_choice == Rock and comp_choice == Paper: #Rock vs Paper
+            comp_score += 1 #Updates score
+            num_rounds += 1 #updates number of rounds effective rounds played
+            print(f"Round {num_rounds} goes to Computer.\nYou: {user_score}\nComputer: {comp_score}\n")
+
         elif user_choice == Rock and comp_choice == Scissors: #Rock vs Scissors
-            user_win_left += - 1
-            user_score += 1
-            print(f"Round 1 goes to You. One more to go!\nYou - {user_score}.\nComputer - {comp_score}.\n")
+            user_score += 1 #Updates score
+            num_rounds += 1
+            print(f"Round {num_rounds} goes to You.\nYou: {user_score}\nComputer: {comp_score}\n")
+
         elif user_choice == Paper and comp_choice == Rock: #Paper vs Rock
-            user_win_left += - 1
-            user_score += 1
-            print(f"Round 1 goes to You. One more to go!\nYou - {user_score}.\nComputer - {comp_score}.\n")
+            user_score += 1 #Updates score
+            num_rounds += 1
+            print(f"Round {num_rounds} goes to You.\nYou: {user_score}\nComputer: {comp_score}\n")
+
         elif user_choice == Paper and comp_choice == Scissors: #Paper vs Scissors
-            comp_win_left += - 1
-            comp_score += 1
-            print(f"Round 1 goes to Computer.\nYou - {user_score}.\nComputer - {comp_score}.\n")
+            comp_score += 1 #Updates score
+            num_rounds += 1
+            print(f"Round {num_rounds} goes to Computer.\nYou: {user_score}\nComputer: {comp_score}\n")
+
         elif user_choice == Scissors and comp_choice == Rock: #Scissors vs Rock
-            comp_win_left += - 1
-            comp_score += 1
-            print(f"Round 1 goes to Computer.\nYou - {user_score}.\nComputer - {comp_score}.\n")
+            comp_score += 1 #Updates score
+            num_rounds += 1
+            print(f"Round {num_rounds} goes to Computer.\nYou: {user_score}\nComputer: {comp_score}\n")
+
         elif user_choice == Scissors and comp_choice == Paper: #Scissors vs Paper
-            user_win_left += - 1
-            user_score += 1
-            print(f"Round 1 goes to You. One more to go!\nYou - {user_score}.\nComputer - {comp_score}.\n")
+            user_score += 1 #Updates score
+            num_rounds += 1
+            print(f"Round {num_rounds} goes to You.\nYou: {user_score}\nComputer: {comp_score}\n")
 
-    if comp_score == 2:
+        else: #Same Combination
+            print(f"Tie. Please Try Again.\nYou: {user_score}\nComputer: {comp_score}\n")
+                
+    if comp_score == (best_of + 1)/2 : #Checks to see if Computer won, else player won
         print("You Lost. Please Try Again!")
-        try_again_response = input("Would you like to play again? (Y/N)").lower()
-        if try_again_response == "n":
-            try_again_track += 1
+        try_again_response = input("Would you like to play again? (Y/N): ").lower()
 
+        while True: #Checks if they entered something other than y or n
+            if try_again_response == "n" or try_again_response == "y":
+                break
+            else:
+                try_again_response = input("Please try again: ").lower()
+        if try_again_response == "n":
+            break
     else:
         print("You Won!")
-        try_again_response = input("Would you like to play again? (Y/N)").lower()
-        if try_again_response == "n":
-            try_again_track += 1
+        try_again_response = input("Would you like to play again? (Y/N): ").lower()
 
-print("\nThank you for playing.")
+        while True: #Checks if they entered something other than y or n
+            if try_again_response == "n" or try_again_response == "y":
+                break
+            else:
+                try_again_response = input("Please try again: ").lower()
+
+        if try_again_response == "n":
+            break
+        
+
+print("\nThank you for playing.\n")
